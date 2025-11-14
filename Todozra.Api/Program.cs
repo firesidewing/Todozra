d1678ddf026cbdd007a66a293a521267793604dc
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2025-11-13 21:23:24 -0800

    Set up database migration and configuration for EF Core with SQLite
    - Added `InitialCreate` migration to define the `Todos` table.
    - Updated `appsettings.json` to include SQLite connection string.
    - Registered `TodoDbContext` with EF Core in `Program.cs` and applied auto-migrations in development environment.
    - Included Entity Framework Core packages for design-time and runtime support.
    - Updated `.gitignore` to exclude `.db` files.
    - Added `.idea` settings for data source and SQL dialect mapping.

--- Todozra.Api/Program.cs
+++ Todozra.Api/Program.cs
@@ -8,12 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDbContext>(options =>
 {
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-    options.UseSqlite("TodozraDb");
+    options.UseSqlite(builder.Configuration.GetConnectionString("TodoDb"));
 });
 
 builder.Services.AddEndpoints(typeof(IEndPoint).Assembly);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<TodoDbContext>();
+        db.Database.Migrate();
+    }
+}
 
 app.MapEndpoints();
 
---
+++ Todozra.Db/Migrations/20251114051740_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todozra.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 2000, nullable: true),
+                    CompletedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
